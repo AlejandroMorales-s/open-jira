c01b4e900ae6f718cf29cd9b8776e056bc9762f4
Alejandro <alejandrom.am33@gmail.com>
2023-03-29 16:10:39 -0600

    added layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,14 @@
 import { FC } from "react";
 import { Typography } from "@mui/material";
+import { Layout } from "@/components/layouts";
 
 const Home: FC = () => {
   return (
-    <Typography variant="h1" color="primary">
-      ola
-    </Typography>
+    <Layout>
+      <Typography variant="h1" color="primary">
+        ola
+      </Typography>
+    </Layout>
   );
 };
 
