a24b67a30eb242157e2393cc11f26fe0f3dc889f
Alejandro <alejandrom.am33@gmail.com>
2023-03-29 20:51:29 -0600

    added containers for tasks

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,32 @@
 import { FC } from "react";
-import { Typography } from "@mui/material";
+import { Grid, Card, CardHeader, CardContent } from "@mui/material";
 import { Layout } from "@/components/layouts";
 
 const Home: FC = () => {
   return (
-    <Layout>
-      <Typography variant="h1" color="primary">
-        ola
-      </Typography>
+    <Layout title="Home - OpenJira">
+      <Grid container spacing={2}>
+        <Grid item xs={12} sm={4}>
+          <Card sx={{ height: "calc(100vh - 100px)" }}>
+            <CardHeader title="Pending" />
+            <CardContent></CardContent>
+          </Card>
+        </Grid>
+
+        <Grid item xs={12} sm={4}>
+          <Card sx={{ height: "calc(100vh - 100px)" }}>
+            <CardHeader title="In Progress" />
+            <CardContent></CardContent>
+          </Card>
+        </Grid>
+
+        <Grid item xs={12} sm={4}>
+          <Card sx={{ height: "calc(100vh - 100px)" }}>
+            <CardHeader title="Completed" />
+            <CardContent></CardContent>
+          </Card>
+        </Grid>
+      </Grid>
     </Layout>
   );
 };
