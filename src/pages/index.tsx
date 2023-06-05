52b0a5e24af4e76b16994e666b90e36be8dc74ad
Alejandro <alejandrom.am33@gmail.com>
2023-06-05 13:54:28 -0600

    added cards by status in the ui

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,31 +1,31 @@
 import { FC } from "react";
-import { Grid, Card, CardHeader, CardContent } from "@mui/material";
+import { Grid, Card, CardHeader } from "@mui/material";
 import { Layout } from "@/components/layouts";
+import { EntryList } from "@/components/ui";
+import { EntryStatus } from "@/interfaces";
+
+interface StatusDetails {
+  statusDetails: { name: string; type: EntryStatus }[];
+}
+
+const statusDetails: StatusDetails["statusDetails"] = [
+  { name: "Pending", type: "pending" },
+  { name: "In Progress", type: "in-progress" },
+  { name: "Completed", type: "finished" },
+];
 
 const Home: FC = () => {
   return (
     <Layout title="Home - OpenJira">
       <Grid container spacing={2}>
-        <Grid item xs={12} sm={4}>
-          <Card sx={{ height: "calc(100vh - 100px)" }}>
-            <CardHeader title="Pending" />
-            <CardContent></CardContent>
-          </Card>
-        </Grid>
-
-        <Grid item xs={12} sm={4}>
-          <Card sx={{ height: "calc(100vh - 100px)" }}>
-            <CardHeader title="In Progress" />
-            <CardContent></CardContent>
-          </Card>
-        </Grid>
-
-        <Grid item xs={12} sm={4}>
-          <Card sx={{ height: "calc(100vh - 100px)" }}>
-            <CardHeader title="Completed" />
-            <CardContent></CardContent>
-          </Card>
-        </Grid>
+        {statusDetails.map((statusData, index) => (
+          <Grid key={index} item xs={12} sm={4} lg={3}>
+            <Card sx={{ height: "calc(100vh - 100px)" }}>
+              <CardHeader title={statusData.name} />
+              <EntryList status={statusData.type} />
+            </Card>
+          </Grid>
+        ))}
       </Grid>
     </Layout>
   );
